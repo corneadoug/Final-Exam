736bfbb0623affc0c698864fdfc999af96fe5d3e
Jcoub1 <jerome.coubronne@gmail.com>
2017-12-08 12:31:56 +0100

    Ajout test type + mauvaise valeur

--- spec/convert.spec.js
+++ spec/convert.spec.js
@@ -5,7 +5,11 @@ describe('Test des parametres de la fonction', function() {
     });
     
     it('les 3 parametres doivent etre du bon type', function() {
-
+		expect(convertisseur('abc', 5,'abc'))..not.toBe(null);
+        expect(convertisseur(5,25,'abc')).toBe(null);
+        expect(convertisseur('acd',17,25)).toBe(null);
+        expect(convertisseur('acd','cbh','vgh')).toBe(null);
+        expect(convertisseur(1,2,3)).toBe(null);
     });
 
 });
@@ -29,6 +33,9 @@ describe('Test resultats de la fonction', function() {
     });
 
     it('ne doit pas convertir des mauvais montants', function() {
-	    
+		expect(convertisseur('USD',2,'EUR')).not.toBe(null);
+        expect(convertisseur('USD',0,'EUR')).toBe(null);
+        expect(convertisseur('USD',,'EUR')).toBe(null);
+        expect(convertisseur('USD',-5,'EUR')).toBe(null);
     });
 });
