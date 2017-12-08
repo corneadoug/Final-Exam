370748e5b020e9a007a311661e19ab6a1c267137
Jcoub1 <jerome.coubronne@gmail.com>
2017-12-08 12:30:57 +0100

    Ajout all

---
+++ spec/convert.spec.js
@@ -0,0 +1,34 @@
+describe('Test des parametres de la fonction', function() {
+	
+	it('la fonction doit prendre 3 parametres', function() {
+
+    });
+    
+    it('les 3 parametres doivent etre du bon type', function() {
+
+    });
+
+});
+
+describe('Test resultats de la fonction', function() {
+	
+	it('doit convertir des USD dans les autres monnaies', function() {
+		
+    });
+    
+    it('doit convertir des EUR dans les autres monnaies', function() {
+
+    });
+
+    it('doit convertir des KRW dans les autres monnaies', function() {
+		
+    });
+
+    it('doit convertir des NZD dans les autres monnaies', function() {
+		
+    });
+
+    it('ne doit pas convertir des mauvais montants', function() {
+	    
+    });
+});
